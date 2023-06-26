efd28899af6124636db9ae11a10f25907790dd60
shin <sshoony95@gmail.com>
2023-06-26 15:37:07 -0400

    Add template

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,145 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import React from "react";
+import { makeStyles } from "@material-ui/core/styles";
+import AppBar from "@material-ui/core/AppBar";
+import Toolbar from "@material-ui/core/Toolbar";
+import Typography from "@material-ui/core/Typography";
+import Button from "@material-ui/core/Button";
+import FormGroup from "@material-ui/core/FormGroup";
+import FormControlLabel from "@material-ui/core/FormControlLabel";
+import Switch from "@material-ui/core/Switch";
+import Grid from "@material-ui/core/Grid";
+import TextField from "@material-ui/core/TextField";
+import Card from "@material-ui/core/Card";
+import CardActions from "@material-ui/core/CardActions";
+import CardContent from "@material-ui/core/CardContent";
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    flexGrow: 1,
+  },
+  container: {
+    "& > *": {
+      margin: theme.spacing(1),
+    },
+  },
+  spacer: {
+    marginTop: 20,
+    marginBottom: 20,
+  },
+  card: {
+    // maxWidth: 400,
+    //  minWidth: 275
+  },
+  bullet: {},
+  title: {},
+  pos: {},
+}));
 
 function App() {
-  const [count, setCount] = useState(0)
+  const classes = useStyles();
+  const [state, setState] = React.useState({
+    checkedA: true,
+    checkedB: true,
+  });
+
+  const handleChange = (event: { target: { name: any; checked: any } }) => {
+    setState({ ...state, [event.target.name]: event.target.checked });
+  };
+
+  const bull = <span className={classes.bullet}>•</span>;
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className={classes.root}>
+        <AppBar position="static">
+          <Toolbar>
+            <Typography variant="h6" className={classes.title}>
+              My React app
+            </Typography>
+          </Toolbar>
+        </AppBar>
+
+        <div className={classes.spacer} />
+
+        <Grid container spacing={5} direction={"column"}>
+          <Grid item xs={12}>
+            <Typography variant="h4">
+              React material UI is{" "}
+              <span role="img" aria-label="heart emoji">
+                ❤️
+              </span>
+            </Typography>
+          </Grid>
+          <Grid item xs={12}>
+            <Card className={classes.card}>
+              <CardContent>
+                <Typography
+                  className={classes.title}
+                  color="primary"
+                  gutterBottom
+                >
+                  Word of the Day
+                </Typography>
+                <Typography variant="h5" component="h2">
+                  be{bull}nev{bull}o{bull}lent
+                </Typography>
+                <Typography className={classes.pos} color="secondary">
+                  adjective
+                </Typography>
+                <Typography variant="body2" component="p">
+                  well meaning and kindly.
+                  <br />
+                  {'"a benevolent smile"'}
+                </Typography>
+              </CardContent>
+              <CardActions>
+                <Button size="small">Learn More</Button>
+              </CardActions>
+            </Card>
+          </Grid>
+          <Grid item xs={12}>
+            <TextField id="outlined-basic" label="Outlined" fullWidth />
+          </Grid>
+          <Grid item xs={12}>
+            <div className={classes.container}>
+              <Button>Default</Button>
+              <Button variant="contained" color="primary">
+                Primary
+              </Button>
+              <Button variant="contained" color="secondary">
+                Secondary
+              </Button>
+            </div>
+          </Grid>
+          <Grid item xs={12}>
+            <FormGroup row>
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={state.checkedB}
+                    onChange={handleChange}
+                    name="checkedB"
+                    color="primary"
+                  />
+                }
+                label="Primary"
+              />
+              <FormControlLabel
+                control={
+                  <Switch
+                    checked={state.checkedA}
+                    onChange={handleChange}
+                    name="checkedA"
+                  />
+                }
+                label="Secondary"
+              />
+            </FormGroup>
+          </Grid>
+        </Grid>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+const rootElement = document.getElementById("root");
+ReactDOM.render(
   <React.StrictMode>
     <App />
   </React.StrictMode>,
-)
+  rootElement
+);
